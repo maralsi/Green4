5f6fd24f7283f0cffeca0c057222934ee0513485
maralsi <maral.sagynalieva@gmail.com>
2024-10-06 17:58:09 +0600

    homework8

--- Green4/settings.py
+++ Green4/settings.py
@@ -93,7 +93,9 @@ WSGI_APPLICATION = 'Green4.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR/'db.sqlite3'
+        'ENGINE': 'django.db.backend.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
